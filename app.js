855f971fe96e7fa053b9d6591fd15da977959a55
Buhle <mtshalizanobuhle7@gmail.com>
2025-02-16 13:19:02 +0200

    updated the files

--- app.js
+++ app.js
@@ -44,3 +44,48 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+
+// CREATING A FUNCTION FOR THE MOVIES/SERIES SLIDESHOW
+let slideIndex = 1;
+window.plusSlides = plusSlides;
+window.currentSlide = currentSlide;
+showSlides(slideIndex);
+
+// next/previous controls
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('slideshow');
+    let dots = document.getElementsByClassName('demo');
+    let captionText = document.getElementById('caption');
+
+
+    if(n > slides.length){
+        slideIndex = 1
+    }
+
+    if(n < 1){
+        slideIndex = slides.length
+    }
+
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt
+}
